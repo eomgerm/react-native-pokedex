058d2d6ec4b57aaf11be1c02bc409b21bf70dc73
eomgerm <fishbread00@gmail.com>
2022-06-24 09:21:39 +0000

    AppColor configuration

---
+++ styles/colors.ts
@@ -0,0 +1,14 @@
+type Colors = {
+  [key: string]: string;
+};
+
+const AppColors: Colors = {
+  teal: "#48a999",
+  red: "#ff6659",
+  blue: "#63a4ff",
+  yellow: "#fbc02d",
+  purple: "#ae52d4",
+  brown: "#8b6b61",
+};
+
+export default AppColors;
