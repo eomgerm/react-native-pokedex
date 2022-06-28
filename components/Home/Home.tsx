42e13601404a56513f608510ea8ec78ef5a53ba3
eomgerm <fishbread00@gmail.com>
2022-06-28 10:01:54 +0000

    animated header added

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,24 +1,78 @@
-import { useRef } from "react";
-import { ScrollView, View, StyleSheet, Animated } from "react-native";
+import { View, StyleSheet, Animated, useWindowDimensions } from "react-native";
+import AppColors from "../../styles/colors";
 import HeaderCategory from "./HeaderCategory";
 import PokemonNews from "./PokemonNews";
+import { useRef } from "react";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackProps } from "../Routes";
 
-const Home = () => {
+type HomeScreenProps = NativeStackNavigationProp<RootStackProps, "Home">;
+
+type Props = {
+  navigation: HomeScreenProps;
+};
+
+const Home = ({ navigation }: Props) => {
+  const { width } = useWindowDimensions();
+  const scrollY = useRef(new Animated.Value(0)).current;
+  const headerOpacity = scrollY.interpolate({
+    inputRange: [350, 500],
+    outputRange: [0, 1],
+    extrapolate: "clamp",
+  });
+
   return (
-    <ScrollView style={{ backgroundColor: "#f5f5f5", flex: 1 }} contentContainerStyle={styles.scrollView}>
-      <View
+    <>
+      <Animated.ScrollView
+        style={styles.scrollView}
+        contentContainerStyle={styles.scrollView}
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: {
+                contentOffset: {
+                  y: scrollY,
+                },
+              },
+            },
+          ],
+          { useNativeDriver: true }
+        )}
+        scrollEventThrottle={16}
+      >
+        <View
+          style={{
+            backgroundColor: "white",
+            height: 1000,
+            position: "absolute",
+            top: -1000,
+            left: 0,
+            right: 0,
+          }}
+        />
+        <HeaderCategory />
+        <PokemonNews />
+      </Animated.ScrollView>
+      <Animated.View
         style={{
-          backgroundColor: "white",
-          height: 1000,
+          backgroundColor: AppColors.red,
+          paddingTop: 65,
+          paddingBottom: 15,
+          alignItems: "center",
+          justifyContent: "center",
+          borderBottomLeftRadius: 30,
+          borderBottomRightRadius: 30,
+          zIndex: 100,
           position: "absolute",
-          top: -1000,
+          top: 0,
           left: 0,
-          right: 0,
+          width,
+          opacity: headerOpacity,
         }}
-      />
-      <HeaderCategory />
-      <PokemonNews />
-    </ScrollView>
+      >
+        <Animated.Text style={{ fontFamily: "CircularStdBlack", fontSize: 24, color: "white" }}>Pokedex</Animated.Text>
+      </Animated.View>
+    </>
   );
 };
 
