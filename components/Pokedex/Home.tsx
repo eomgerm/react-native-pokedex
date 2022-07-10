75c50b1865252808cba8855d72e46be8ee140a1f
eomgerm <fishbread00@gmail.com>
2022-07-10 06:29:38 +0000

    axios type added

--- components/Pokedex/Home.tsx
+++ components/Pokedex/Home.tsx
@@ -3,7 +3,7 @@ import PokeballBackground from "../commons/PokeballBackground";
 import { useEffect, useState } from "react";
 import FAB from "./FAB/FAB";
 import axios from "axios";
-import { Result } from "../../types/pokemon_api_result";
+import PokemonApiResult, { Result } from "../../types/pokemon_api_result";
 import Pokemon from "../../types/pokemon";
 import PokemonCard from "./PokemonCard";
 import Header from "./Header";
@@ -13,10 +13,10 @@ const Home = () => {
 
   useEffect(() => {
     (async () => {
-      const { data } = await axios("https://pokeapi.co/api/v2/pokemon/");
+      const { data } = await axios.get<PokemonApiResult>("https://pokeapi.co/api/v2/pokemon/");
       const pokemonsArr = await Promise.all(
         data.results.map(async (result: Result) => {
-          const { data: pokemonData } = await axios(result.url);
+          const { data: pokemonData } = await axios.get<Pokemon>(result.url);
           return pokemonData;
         })
       );
