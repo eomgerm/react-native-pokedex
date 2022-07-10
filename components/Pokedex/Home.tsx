f9f5d5ef0d0ce9e0791dd092900086e2f5351b67
eomgerm <fishbread00@gmail.com>
2022-07-10 08:13:39 +0000

    useful features added

--- components/Pokedex/Home.tsx
+++ components/Pokedex/Home.tsx
@@ -1,28 +1,48 @@
-import { View, FlatList } from "react-native";
+import { View, FlatList, Image } from "react-native";
 import PokeballBackground from "../commons/PokeballBackground";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import FAB from "./FAB/FAB";
 import axios from "axios";
 import PokemonApiResult, { Result } from "../../types/pokemon_api_result";
 import Pokemon from "../../types/pokemon";
 import PokemonCard from "./PokemonCard";
 import Header from "./Header";
+import Loading from "../commons/Loading";
 
 const Home = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [offset, setOffset] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [refreshing, setRefreshing] = useState<boolean>(false);
 
-  useEffect(() => {
-    (async () => {
-      const { data } = await axios.get<PokemonApiResult>("https://pokeapi.co/api/v2/pokemon/");
+  const loadPokemons = useCallback(
+    async (offsetValue = offset, shouldRefresh = false) => {
+      setLoading(true);
+      const { data } = await axios.get<PokemonApiResult>(`https://pokeapi.co/api/v2/pokemon/?offset=${offsetValue}`);
       const pokemonsArr = await Promise.all(
         data.results.map(async (result: Result) => {
           const { data: pokemonData } = await axios.get<Pokemon>(result.url);
           return pokemonData;
         })
       );
-      setPokemons(pokemonsArr);
-    })();
+      setOffset(shouldRefresh ? 20 : (prev) => prev + 20);
+      setPokemons(shouldRefresh ? pokemonsArr : [...pokemons, ...pokemonsArr]);
+      setLoading(false);
+    },
+    [pokemons, setLoading]
+  );
+
+  const refreshPokemons = useCallback(async () => {
+    setRefreshing(true);
+    loadPokemons(0, true);
+    setRefreshing(false);
   }, []);
+
+  useEffect(() => {
+    loadPokemons();
+  }, []);
+
+  const ListFooterComponent = useMemo(() => (loading ? <Loading /> : null), [loading]);
 
   return (
     <View style={{ flex: 1, paddingTop: 50 }}>
@@ -34,6 +54,12 @@ const Home = () => {
         contentContainerStyle={{ paddingBottom: 24, paddingHorizontal: 24 }}
         keyExtractor={(pokemon) => String(pokemon.id)}
         numColumns={2}
+        showsVerticalScrollIndicator={false}
+        onEndReached={() => loadPokemons()}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={ListFooterComponent}
+        onRefresh={() => refreshPokemons()}
+        refreshing={refreshing}
         renderItem={({ item: pokemon }) => <PokemonCard pokemon={pokemon} />}
       />
       <FAB />
