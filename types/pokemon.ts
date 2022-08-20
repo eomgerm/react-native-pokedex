ca51d330200c50f85c0a19261f9f3e21cbfeb79f
eomgerm <fishbread00@gmail.com>
2022-08-20 17:18:41 +0000

    Pokemon type redesigned

--- types/pokemon.ts
+++ types/pokemon.ts
@@ -1,75 +1,26 @@
-export type Ability = {
-  ability: {
-    name: string;
-    url: string;
-  };
-  is_hidden: boolean;
-  slot: number;
-};
-
-export type Form = {
+export type Pokemon = {
+  id: number;
   name: string;
-  url: string;
-};
-
-export type GameIndice = {
-  game_index: number;
-  version: {
-    name: string;
-    url: string;
-  };
-};
-
-export type Move = {
-  move: {
-    name: string;
-    url: string;
-  };
+  types: Type[];
+  sprites: string;
+  genera: string;
+  flavor_text: string;
+  height: number;
+  weight: number;
+  gender_rate: number;
+  base_experience: number;
+  egg_groups: string[];
+  stats: Stat[];
 };
 
 export type Stat = {
+  name: string;
   base_stat: number;
-  effort: number;
-  stat: {
-    name: "hp" | "attack" | "defense" | "special-attack" | "special-defense" | "speed";
-    url: string;
-  };
 };
 
 export type Type = {
-  slot: number;
-  type: {
-    name: string;
-    url: string;
-  };
-};
-
-export type Pokemon = {
-  abilities: Ability[];
-  base_experience: number;
-  forms: Form[];
-  game_indices: GameIndice[];
-  height: number;
-  id: number;
-  is_default: boolean;
-  location_area_encounters: string;
-  moves: Move[];
   name: string;
-  order: number;
-  species: {
-    name: string;
-    url: string;
-  };
-  sprites: {
-    other: {
-      "official-artwork": {
-        front_default: string;
-      };
-    };
-  };
-  stats: Stat[];
-  types: Type[];
-  weight: number;
+  slot: number;
 };
 
 export default Pokemon;
